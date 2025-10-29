a56cd89e1ebf9d78b21d7f4541843174f9ec2f7e
Theepicosity <theepicadrian@gmail.com>
2025-10-28 23:06:02 -0700

    add commands to subscribe channels to leaderboard notifications

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import json
 import requests
 import urllib.parse
 import discord
+from typing import Optional
+from discord import app_commands
 from discord.ext import tasks
 
 REFRESH_TIME = 60 # seconds
 SCORES_THRESHOLD = 4
 EDIT_TIME = 900 # improvements within 15 minutes of each other result in an edited message instead of a new one, to reduce spam
-LB_CHANNEL_ID = 412356109018595329
+LB_CHANNEL_ID = 811782756996087858
+GUILD_ID = 435308083036553217 # used for guild-specific commands
 LB_API_SERVER = "https://openhexagon.fun:8001"
 
 def rreplace(s, old, new):
@@ -26,9 +29,64 @@ class leaderboard_client(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    async def command_tree(self):
+        tree = app_commands.CommandTree(self)
+        guild = discord.Object(id=GUILD_ID)
+
+        # TODO: allow not hardcoded guild id (probably with some kind of init message) maybe the open hexagon guild and channel can be hardcoded, or a default in saved_state.json
+        # global recent command
+
+        @tree.command(name="subscribe", description="Subscribes this channel to leaderboard updates", guild=guild)
+        async def guild_subscribe(interaction: discord.Interaction):
+            # i think there's a better way to do the permissions handling but this is quick and easy
+            if interaction.permissions.manage_guild == True:
+                await self.update_subscribed_channels(dict(channel_id=interaction.channel.id, guild_id=interaction.guild.id), False)
+                await interaction.response.send_message("This channel has subscribed to leaderboard updates.")
+            else:
+                await interaction.response.send_message("You do not have permissions to run this command.", ephemeral=True)
+
+        @tree.command(name="unsubscribe", description="Unsubscribes this channel from leaderboard updates", guild=guild)
+        async def guild_unsubscribe(interaction: discord.Interaction):
+            if interaction.permissions.manage_guild == True:
+                await self.update_subscribed_channels(dict(channel_id=interaction.channel.id, guild_id=interaction.guild.id), True)
+                await interaction.response.send_message("This channel has unsubscribed from leaderboard updates.")
+            else:
+                await interaction.response.send_message("You do not have permissions to run this command.", ephemeral=True)
+
+        self.tree = tree
+
+    async def update_subscribed_channels(self, new_entry, is_removing_entry):
+        saved_state = {}
+        try:
+            with open("saved_state.json") as fp:
+                saved_state = json.load(fp)
+        except FileNotFoundError:
+            pass  # just uses defaults
+        saved_state["video_queue"] = saved_state.get("video_queue", [])
+        saved_state["last_call_timestamp"] = saved_state.get("last_call_timestamp", 0)
+        saved_state["recent_scores"] = saved_state.get("recent_scores", [])
+        saved_state["subscribed_channels"] = saved_state.get("subscribed_channels", [])
+
+        if is_removing_entry:
+            # find the right channel id and remove it
+            for entry in saved_state["subscribed_channels"]:
+                if entry["channel_id"] == new_entry["channel_id"]:
+                    saved_state["subscribed_channels"].remove(entry)
+        else:
+            saved_state["subscribed_channels"].append({"channel_id": new_entry["channel_id"], "guild_id": new_entry["guild_id"]})
+
+        with open("saved_state.json", "w") as fp:
+            json.dump(saved_state, fp)
+        log("Updated subscribed channels.")
+
     async def on_ready(self):
         assert isinstance(self.user, discord.ClientUser)
         log(f"Logged in as {self.user} (ID: {self.user.id})")
+
+        # setup command tree
+        await self.command_tree()
+        await self.tree.sync(guild=discord.Object(id=GUILD_ID))
+        log(f"Synchronized commands with guild. (GUILD_ID: {GUILD_ID})")
 
     async def setup_hook(self) -> None:
         # setup pack and level lookup table
@@ -49,6 +107,7 @@ class leaderboard_client(discord.Client):
         saved_state["video_queue"] = saved_state.get("video_queue", [])
         saved_state["last_call_timestamp"] = saved_state.get("last_call_timestamp", 0)
         saved_state["recent_scores"] = saved_state.get("recent_scores", [])
+        saved_state["subscribed_channels"] = saved_state.get("subscribed_channels", [])
 
         current_time = time.time()
         time_difference = math.ceil(current_time - saved_state["last_call_timestamp"])
@@ -60,13 +119,14 @@ class leaderboard_client(discord.Client):
         scores_json = recent_scores.json()
         log(f"{len(scores_json)} scores found.")
         await self.send_wrs(scores_json, saved_state)
-        await self.check_videos(saved_state["video_queue"])
+        await self.check_videos(saved_state["video_queue"], saved_state["subscribed_channels"])
         with open("saved_state.json", "w") as fp:
             json.dump(saved_state, fp)
         log("Done.")
 
     async def send_wrs(self, scores_json, saved_state):
-        channel = self.get_output_channel()
+        # note: the channels are only checked at the start of the loop, meaning that scores could be sent even after running the unsubscribe command
+        channels = self.get_output_channels(saved_state["subscribed_channels"])
         for score in scores_json:
             rank = score["position"]
 
@@ -115,13 +175,14 @@ class leaderboard_client(discord.Client):
                     if pack_name[0] == "#":
                         pack_name = "\\" + pack_name
 
-                    score_text = f"**{pack_name} - {level_name}{diff_str}** <:hexagon:1388672832094867486> **{player}** achieved **#{rank}** with a score of **{run_length}**"
+                    score_text = f"**{pack_name} - {level_name}{diff_str}** <:hexagon:1432891341297418322> **{player}** achieved **#{rank}** with a score of **{run_length}**"
 
                     # remove old messages from the edit queue
                     for last_score in saved_state["recent_scores"]:
                         if score["timestamp"] - last_score["timestamp"] > EDIT_TIME:
                             saved_state["recent_scores"].remove(last_score)
 
+                    messages = []
                     edited = False
                     # check if score could be edited into a previous message 
                     for last_score in saved_state["recent_scores"]:
@@ -134,26 +195,34 @@ class leaderboard_client(discord.Client):
                                 saved_state["recent_scores"].remove(last_score)
                                 break
 
-                            msg = await channel.fetch_message(last_score["message_id"])
+                            for channel in channels:
+                                for message in last_score["messages"]:
+                                    if channel.id == message["channel_id"]:
+                                        msg = await channel.fetch_message(message["message_id"])
 
-                            new_content = msg.content + "\n" + score_text
+                                        new_content = msg.content + "\n" + score_text
 
-                            log(f"Appending '{score_text}' to message {msg.id}")
-                            await msg.edit(content=new_content)
+                                        log(f"Appending '{score_text}' to message {msg.id}")
+                                        await msg.edit(content=new_content)
 
-                            edited = True
+                                        messages.append({"channel_id": channel.id, "message_id": msg.id})
+                                        edited = True
+                                        break
                             break
                     
                     if not edited:
+                        messages = []
                         # send new message
-                        msg = await channel.send(score_text)
-                        saved_state["recent_scores"].append({**score, "message_id": msg.id})
+                        for channel in channels:
+                            msg = await channel.send(score_text)
+                            messages.append({"channel_id": channel.id, "message_id": msg.id})
+                        saved_state["recent_scores"].append({**score, "messages": messages})
                     
                     if rank == 1:
-                        saved_state["video_queue"].append({**score, "message_id": msg.id})
+                        saved_state["video_queue"].append({**score, "messages": messages})
 
-    async def check_videos(self, queue):
-        channel = self.get_output_channel()
+    async def check_videos(self, queue, subscribed_channels):
+        channels = self.get_output_channels(subscribed_channels)
         log(f"Checking {len(queue)} queued messages for video progress.")
         while len(queue) > 0:
             score = queue[0]
@@ -179,17 +248,21 @@ class leaderboard_client(discord.Client):
                 return
             if response_headers["Content-Type"] == "video/mp4":
                 # exists now, edit message to include link
-                message = await channel.fetch_message(score["message_id"])
-                run_length = round(score["value"], 3)
+                for channel in channels:
+                    for message in score["messages"]:
+                        if channel.id == message["channel_id"]:
+                            msg = await channel.fetch_message(message["message_id"])
+                            run_length = round(score["value"], 3)
 
-                # remove previous links
-                new_content = re.sub(r"\[(\d+(\.\d*)?)\]\(.+\)", r"\1", message.content)
-                # add newest link
-                new_content = rreplace(new_content, f"**{run_length}**", f"**[{run_length}]({video_link}) **")
+                            # remove previous links
+                            new_content = re.sub(r"\[(\d+(\.\d*)?)\]\(.+\)", r"\1", msg.content)
+                            # add newest link
+                            new_content = rreplace(new_content, f"**{run_length}**", f"**[{run_length}]({video_link}) **")
 
-                log(f"Editing '{message.content}' to '{new_content}'")
-                await message.edit(content=new_content)
-                queue.pop(0)
+                            log(f"Editing '{msg.content}' to '{new_content}'")
+                            await msg.edit(content=new_content)
+                            queue.pop(0)
+                            break
             else:
                 return
 
@@ -197,13 +270,16 @@ class leaderboard_client(discord.Client):
     async def before_my_task(self):
         await self.wait_until_ready()  # wait until the bot logs in
     
-    def get_output_channel(self):
-        channel = self.get_channel(LB_CHANNEL_ID)
-        if not channel:
-            log(f"ERROR: Could not find channel <{LB_CHANNEL_ID}>.")
-            return
-        assert isinstance(channel, discord.TextChannel), "You have set your output to a channel that isn't a text channel."
-        return channel
+    def get_output_channels(self, subscribed_channels):
+        output_channels = []
+        for entry in subscribed_channels:
+            channel = self.get_channel(entry["channel_id"])
+            if not channel:
+                log(f"ERROR: Could not find channel <{LB_CHANNEL_ID}>.")
+                return
+            assert isinstance(channel, discord.TextChannel), "You have set your output to a channel that isn't a text channel."
+            output_channels.append(channel)
+        return output_channels
 
     def create_lookup_table(self):
         all_packs = requests.get(f"{LB_API_SERVER}/get_packs/1/1000")
